f02933338406383910a7c220080cd1b61fb5e7c3
Michael Jones <djyoungjones@gmail.com>
2020-04-19 23:25:37 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,4 @@
-deff mario
+def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
  phrase = "It's-a me, Mario!"
   
@@ -14,8 +14,8 @@ def link
   phrase = "It's Dangerous To Go Alone! Take This."
  
  puts phrase 
- phrase
 end 
+
 
 
 
